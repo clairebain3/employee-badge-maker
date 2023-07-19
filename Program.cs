471a6f04c8d3a463e12b27d67790c5396d627c5d
Claire Bain <claire.bain3@gmail.com>
2023-07-18 20:27:23 -0700

    moving files to correct repo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+
+
+
+async static Task Main(string[] args)
+{
+  Console.WriteLine("Please enter yes, if you would like to enter data manually: ");
+  string manual = Console.ReadLine() ?? "";
+  if (manual == "yes")
+  {
+    List<Employee>  manualEmp = PeopleFetcher.GetEmployees();
+     Util.PrintEmployees(manualEmp);
+    Util.MakeCSV(manualEmp);
+    await Util.MakeBadges(manualEmp);
+  }
+    List<Employee>  employees = await PeopleFetcher.GetFromApi();
+    Util.PrintEmployees(employees);
+    Util.MakeCSV(employees);
+    await Util.MakeBadges(employees);
+}
+
+  }
+}
